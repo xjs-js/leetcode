2782df45b6c24a81f4646ff569f2f4a0ff53462d
xjs-js <xjs.js@outlook.com>
2021-03-31 20:45:40 +0800

    添加注释

--- 0354.Russian.Doll.Envelopes/sol.cpp
+++ 0354.Russian.Doll.Envelopes/sol.cpp
@@ -3,27 +3,33 @@
  * @follow-up: 刚刚看完小米的发布会，雷总要开始造车了，祝好
  */
 
-#include "../headers.h"
 
 class Solution {
 public:
     /**
      * 看上去像是简单的贪心算法
+     * dp[i][0] 表示第i个不拿，所能达到的最大数量
+     * dp[i][1] 表示第i个一定拿，所能达到的最大数量
+     *
+     * 状态转移方程
+     * dp[i][0] = max(dp[i-1][0], dp[i-1][1]);
+     * dp[i][1] = max{dp[j][1] + 1}, where envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1]
+     *
+     * 最后返回 max(dp[i][0], dp[i][1]);
      */
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         // 什么都不管，上来先排序
         sort(envelopes.begin(), envelopes.end());
 
-        // for (size_t i = 0; i < envelopes.size(); ++i) {
-        //     cout << envelopes[i][0] << " " << envelopes[i][1] << endl;
-        // }
-
         int N = envelopes.size();
+
+        // new space
         int** dp = new int*[N];
         for (int i = 0; i < N; ++i) {
             dp[i] = new int[2];
         }
 
+        // 初始状态
         dp[0][0] = 0;
         dp[0][1] = 1;
 
@@ -41,22 +47,13 @@ public:
         }
 
         int result = max(dp[N-1][0], dp[N-1][1]);
+
+        // delete space
         for (int i = 0; i < N; ++i) {
             delete[] dp[i];
         }
-
         delete[] dp;
 
         return result;
     }
 };
-
-int main(int argc, char* argv[]) {
-    Solution sol;
-    //vector<vector<int> > vec{{2,100},{3,200},{4,300},{5,500},{5,400},{5,250},{6,370},{6,360},{7,380}};
-    // vector<vector<int> > vec{{1,3},{3,5},{6,7},{6,8},{8,4},{9,5}};
-    vector<vector<int> > vec{{46,89}, {50,53}, {52,68}, {72,45}, {77,81}};
-    cout << sol.maxEnvelopes(vec) << endl;
-
-    return 0;
-}
