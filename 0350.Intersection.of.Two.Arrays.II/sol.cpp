98610ebe05fd753f79f74f5e5868ead1eed19bca
xjs-js <xjs.js@outlook.com>
2021-04-15 23:03:15 +0800

    我是sb

---
+++ 0350.Intersection.of.Two.Arrays.II/sol.cpp
@@ -0,0 +1,21 @@
+/**
+ * @date: 2021-04-15 23:02 Thur.
+ */
+ 
+ 
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> left(nums1.begin(), nums1.end());
+        
+        unordered_set<int> result;
+        for (int i = 0; i < nums2.size(); ++i) {
+            if (left.count(nums2[i])) {
+                result.insert(nums2[i]);
+            }
+        }
+        
+        vector<int> rlt(result.begin(), result.end());
+        return rlt;
+    }
+};
