341a713ad80d9bf87229a66ba96ba52692bc7c54
xjs-js <xjs.js@outlook.com>
2021-04-22 16:47:05 +0800

    修正错误

--- 0304.Range.Sum.Query.2D.Immutable/sol.cpp
+++ 0304.Range.Sum.Query.2D.Immutable/sol.cpp
@@ -1,53 +1,53 @@
 /**
  * @date: 2021-04-01 20:29
  * @note: 第303题目偷鸡取巧了，这题要认真
+ * @update: 2021-04-20 这题目做的不对，应该用前缀和的
  */
 
 
 class NumMatrix {
 private:
-    int** sum;
-    vector<vector<int>> _matrix;
+    int** _sum;
 public:
     NumMatrix(vector<vector<int>>& matrix) 
-        : _matrix(matrix)
     {
         int row = matrix.size();
         int col = matrix[0].size();
 
-        sum = new int*[row];
+        _sum = new int*[row];
         for (size_t i = 0; i < row; ++i) {
-            sum[i] = new int[col];
-        }
-
-
-        for (size_t i = 0; i < row; ++i) {
-            int acc = 0;
-            for (size_t j = 0; j < col; ++j) {
-                acc += matrix[i][j];
-                sum[i][j] = acc;
+            _sum[i] = new int[col];
+            for (int j = 0; j < col; ++j) {
+                _sum[i][j] = 0;
             }
         }
+
+        _sum[0][0] = matrix[0][0];
+        for (size_t j = 1; j < col; ++j) {
+            _sum[0][j] = _sum[0][j-1] + matrix[0][j];
+        }
+
+        for (size_t i = 1; i < row; ++i) {
+            _sum[i][0] = _sum[i-1][0] + matrix[i][0];
+        }
+
+        for (size_t i = 1; i < row; ++i) {
+            for (size_t j = 1; j < col; ++j) {
+                _sum[i][j] = _sum[i][j-1] + _sum[i-1][j] - _sum[i-1][j-1] + matrix[i][j];
+            }
+        }
+        
+        
+    }
+    
+    int pre_sum(int i, int j) {
+        if (i < 0 || j < 0) {
+            return 0;
+        }
+        return _sum[i][j];
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int result = 0;
-        for (size_t i = row1; i <= row2; ++i) {
-            result += sum[i][col2] - sum[i][col1] + _matrix[i][col1];
-        }
-
-        return result;
+        return pre_sum(row2, col2) - pre_sum(row2, col1-1) - pre_sum(row1-1, col2) + pre_sum(row1-1, col1-1);
     }
 };
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix* obj = new NumMatrix(matrix);
- * int param_1 = obj->sumRegion(row1,col1,row2,col2);
- */
-
-
-int main(int argc, char* argv[])
-{
-
-}
