cceecff9d162127e386a46e19ba9943c66f84a28
xjs-js <xjs-js@users.noreply.github.com>
2023-11-20 01:19:18 +0800

    GAP = 6 years

--- 0217.Contains.Duplicate/sol.cpp
+++ 0217.Contains.Duplicate/sol.cpp
@@ -1,3 +1,21 @@
+// 2023-11-20
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> count;
+
+        for (int & num : nums) {
+            ++count[num];
+            if (count[num] >= 2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+
 // 2017-05-29
 
 
