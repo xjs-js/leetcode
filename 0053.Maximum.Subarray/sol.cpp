08e066f2ce23b13a1a62ce11116366f0deb17c88
xjs-js <xjs-js@users.noreply.github.com>
2023-11-25 15:26:42 +0800

    Update sol.cpp

--- 0053.Maximum.Subarray/sol.cpp
+++ 0053.Maximum.Subarray/sol.cpp
@@ -1,3 +1,27 @@
+// 2023-11-20
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int N = nums.size();
+        if (N == 0) return 0;
+
+        int maxRelt = nums[0];
+        int preRelt = nums[0];
+        for (int i = 1; i < N; ++i) {
+            int curNum = nums[i];
+
+            if (preRelt + curNum > curNum) {
+                preRelt = preRelt + curNum;
+            } else {
+                preRelt = curNum;
+            }
+
+            maxRelt = max(maxRelt, preRelt);
+        }
+        return maxRelt;
+    }
+};
+
 #include <bits/stdc++.h>
 
 class Solution {
