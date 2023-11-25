5a469332eae72b0f6303d81d379cb9c61ebf53a9
xjs-js <xjs-js@users.noreply.github.com>
2023-11-25 19:58:05 +0800

    Update sol.cpp

--- 0056.Merge.Intervals/sol.cpp
+++ 0056.Merge.Intervals/sol.cpp
@@ -1,3 +1,39 @@
+// 2023/11/23 20:17
+class Solution {
+private:
+    string getHash(string& str) {
+        string relt;
+        vector<int> count(26, 0);
+        for (char c : str)
+            ++count[c - 'a'];
+        
+        for (int i = 0; i < 26; ++i) {
+            int j = count[i];
+            while(j--) {
+                relt += ('a' + i);
+            }
+        }
+        return relt;
+    }
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> group;
+
+        for (string& str : strs) {
+            string hashStr = getHash(str);
+            group[hashStr].push_back(str);
+        }
+
+        auto it = group.begin();
+        vector<vector<string>> relt;
+        while (it != group.end()) {
+            relt.push_back(it->second);
+            ++it;
+        }
+        return relt;
+    }
+};
+
 /**
  * @date: 2021-04-18 23:22
  */
