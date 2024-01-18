c0afb1b837f62135fc898a2f856797c385035867
xjs-js <xjs.js@outlook.com>
2024-01-18 10:41:34 +0800

    New Commit

--- 0278.First.Bad.Version/sol.cpp
+++ 0278.First.Bad.Version/sol.cpp
@@ -32,3 +32,24 @@ public:
         return _versionNum;
     }
 };
+
+/**
+ * @date: 2024-01-18 10:41
+ */
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long l = 1, r = n;
+
+        while (l < r) {
+            long long mid = (l + r) / 2;
+            if (isBadVersion(mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return r;
+    }
+};
