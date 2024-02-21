a000f92c4431e46549316b503ad83a9e42000871
xjs-js <xjs.js@outlook.com>
2024-02-21 21:57:06 +0800

    not as well as last

--- 0106.Construct.Binary.Tree.from.Inorder.and.Postorder.Traversal/sol.cpp
+++ 0106.Construct.Binary.Tree.from.Inorder.and.Postorder.Traversal/sol.cpp
@@ -9,6 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// @date: 2024-02-21.Wed.21:55
+class Solution {
+public:
+    int getIndex(vector<int>& arr, int val) {
+        for (int i = 0; i < arr.size(); ++i) {
+            if (arr[i] == val) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if (inorder.empty() || postorder.empty() || inorder.size() != postorder.size()) {
+            return nullptr;
+        }
+
+        int lastElement = postorder.back();
+        int lastElementIndexInInorder = getIndex(inorder, lastElement);
+        if (TreeNode* root = new TreeNode(lastElement)) {
+            vector<int> leftInOrder(inorder.begin(), inorder.begin() + lastElementIndexInInorder);
+            vector<int> rightInOrder(inorder.begin() + lastElementIndexInInorder + 1, inorder.end());
+
+            int leftInOrderLen = leftInOrder.size();
+            int rightInOrderLen = rightInOrder.size();
+
+            vector<int> leftPostOrder(postorder.begin(), postorder.begin() + leftInOrderLen);
+            vector<int> rightPostOrder(postorder.begin() + leftInOrderLen, postorder.begin() + leftInOrderLen + rightInOrderLen);
+
+            root->left = buildTree(leftInOrder, leftPostOrder);
+            root->right = buildTree(rightInOrder, rightPostOrder);
+
+            return root;
+        }
+        
+        return nullptr;
+    }
+};
+
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, int inBeg, int inEnd, vector<int>& postorder, int postBeg, int postEnd) {
